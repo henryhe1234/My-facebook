3105a56bdbe7e243568a9afb604269d72187e560
henryhe1234 <henryhe1123@gmail.com>
2021-05-12 12:13:10 -0600

    database setup

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const { ApolloServer } = require("apollo-server");
 const gql = require("graphql-tag");
-
+const mongoose = require("mongoose");
+const { MONGODB } = require("./config.js");
 const typeDefs = gql`
   type Query {
     sayHi: String!
@@ -16,6 +17,12 @@ const server = new ApolloServer({
   typeDefs,
   resolvers,
 });
-server.listen({ port: 5000 }).then((res) => {
-  console.log(`Server sunning at ${res.url}`);
-});
+mongoose
+  .connect(MONGODB, { useNewUrlParser: true })
+  .then(() => {
+    console.log("MongoDB connected");
+    return server.listen({ port: 5000 });
+  })
+  .then((res) => {
+    console.log(`Server sunning at ${res.url}`);
+  });

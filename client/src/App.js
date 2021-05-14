3fe895550b62d438c4b35f4c3d8097bb0da4da1c
henryhe1234 <henryhe1123@gmail.com>
2021-05-13 18:20:18 -0600

    routing

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,25 @@
+import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import { Container } from "semantic-ui-react";
+import "semantic-ui-css/semantic.min.css";
 import "./App.css";
 
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import MenuBar from "./components/MenuBar";
+
 function App() {
-  return <div>Hi World!</div>;
+  return (
+    <Router>
+      <Container>
+        <MenuBar />
+        <Route exact path="/" component={Home} />
+        <Route exact path="/login" component={Login} />
+        <Route exact path="/register" component={Register} />
+      </Container>
+    </Router>
+  );
 }
 
 export default App;
